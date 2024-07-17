a454359f5ef873ded718e7431e2817e7618cb541
Rishabh Raj <86614300+Raj-Rishabh@users.noreply.github.com>
2024-07-17 12:02:05 +0530

    Update App.jsx
    Added protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import Form from '../components/Login/LoginForm'
 import EForm from '../components/EmployeeForm/CreateEmployeeForm'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation, Navigate, Outlet } from 'react-router-dom'
 import Dashboard from '../components/Dashboard/Dashboard'
 import NavBar from '../components/NavBar/NavBar'
 import EmployeeList from '../components/EmployeeList/EmployeeList'
 import SignupForm from '../components/SignupPage/SignUp'
 
+const PrivateRoute = ({ isAuthenticated }) => {
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
+
 function App() {
   const { pathname } = useLocation();
   const showNav = !['/signUp', '/'].includes(pathname);
+  const isAuthenticated = !!localStorage.getItem('FullName');
+  
   return (
     <div>
-      { showNav && <NavBar/> }
+      {showNav && <NavBar/>}
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/employeeList" element={<EmployeeList />}/>
-        <Route path="/employee/" element={<EForm/>}/>
-        <Route path="/employee/:id" element={<EForm/>}/>
         <Route path="/" element={<Form/>}/>
         <Route path="/signUp" element={<SignupForm/>}/>
+        <Route element={<PrivateRoute isAuthenticated={isAuthenticated}/>}>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/employeeList" element={<EmployeeList/>}/>
+          <Route path="/employee" element={<EForm/>}/>
+          <Route path="/employee/:id" element={<EForm/>}/>
+        </Route>
       </Routes>
     </div>
   )
