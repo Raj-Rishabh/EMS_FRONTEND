00bb5a9f5b444c8318b5d36e047d7c845f42a6ec
Rishabh Raj <86614300+Raj-Rishabh@users.noreply.github.com>
2024-07-17 12:02:53 +0530

    Update NavBar.jsx
    Added protected routes function

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -4,18 +4,21 @@ import styles from "./NavBar.module.css";
 import pic from "../../src/assets/profile.svg"
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const name = localStorage.getItem('FullName');
 
-    const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/')
+  }
 
-    const name = localStorage.getItem('FullName');
   return (
-      <div className={styles.nav}>
-        <img src={pic} alt="img" />
-        <h3 className={styles.navC} onClick={() => navigate('/dashboard')}>Home</h3>
-        <h3 className={styles.navC} onClick={() => navigate('/employeeList')}>Employee List</h3>
-        <h3 className={styles.name}>{name}</h3>
-        <h3 className={styles.navC} onClick={() => navigate('/')}>Logout</h3>
-      </div>
-    
+    <div className={styles.nav}>
+      <img src={pic} alt="img"/>
+      <h3 className={styles.navC} onClick={() => navigate('/dashboard')}>Home</h3>
+      <h3 className={styles.navC} onClick={() => navigate('/employeeList')}>Employee List</h3>
+      <h3 className={styles.name}>{name}</h3>
+      <h3 className={styles.navC} onClick={handleLogout}>Logout</h3>
+    </div>
   );
 }
